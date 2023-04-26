72f4d3510d03a764fa6c2cc3e8ae7b0f8e7bd0fb
TekniqueJoseph <joejacksun@gmail.com>
2023-04-26 19:01:31 -0400

    added course code

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const startGameBtn = document.getElementById('start-game-btn');
+let h1 = document.querySelector('#heading')
+
+
+function startGame() {
+    console.log('Game is starting...')
+    h1.innerHTML = 'Hello'
+}
+
+startGame()
