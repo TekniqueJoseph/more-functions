20e356beaa576fe7b3430f5802bc544a438f4740
TekniqueJoseph <joejacksun@gmail.com>
2023-04-27 19:32:01 -0400

    added logic for game

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
-let h1 = document.querySelector('#heading');
-let h2 = document.querySelector('#sub');
+let heading = document.querySelector('#heading');
+let player = document.querySelector('#player');
+let computer = document.querySelector('#computer');
+let champion = document.querySelector('#champion')
 
-const ROCK = 'ROCK'
-const PAPER = 'PAPER'
-const SCISSORS = 'SCISSORS'
-const DEFAULT_USER_CHOICE = ROCK
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 let gameIsRunning = false;
 
 const getPlayerChoice = function () {
@@ -20,14 +25,44 @@ const getPlayerChoice = function () {
     return selection
 };
 
+const getComputerChoice = function() {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if ( randomValue < 0.67){
+        return PAPER;
+    } else {
+        return SCISSORS
+    }
+};
+
+const getWinner = function(cChoice, pChoice) {
+    if (cChoice === pChoice) {
+        return RESULT_DRAW;
+    } else if (
+        (cChoice === ROCK && pChoice === PAPER) || 
+        (cChoice === PAPER && pChoice === SCISSORS) ||
+        (cChoice === SCISSORS && pChoice === ROCK)
+    ) {
+        return RESULT_PLAYER_WINS
+    } else {
+        return RESULT_COMPUTER_WINS
+    }
+
+}
+
 startGameBtn.addEventListener('click', function() {
     if(gameIsRunning) {
         return
     }
     gameIsRunning = true
     console.log('The game is starting...')
-    const playerSelection = getPlayerChoice()
-    console.log(playerSelection)
-    h1.innerHTML = 'The game is starting...'
-    h2.innerHTML = `${playerSelection} it is...`
+    const playerChoice = getPlayerChoice()
+    const computerChoice = getComputerChoice()
+    heading.innerHTML = 'The game is starting...'
+    player.innerHTML = `You chose ${playerChoice}.`
+    computer.innerHTML = `The computer chose ${computerChoice}.`
+    const winner = getWinner(computerChoice, playerChoice)
+    console.log(winner)
+    champion.innerHTML = `The Result Is ${winner}!`
 });
